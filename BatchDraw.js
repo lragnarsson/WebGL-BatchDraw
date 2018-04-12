cec53ae4a3e840a5eb6304684c713e0bd82099b4
Lage Ragnarsson <lage.ragnarsson@niradynamics.se>
2018-04-12 14:54:53 +0200

    Functional mercator projection using 64 bit precision sine function from luma.gl.
    Change-Id: I89d573d0abb899d247d504e204a3861812293b29

--- BatchDraw.js
+++ BatchDraw.js
@@ -221,6 +221,9 @@ class BatchDrawer {
         if (this.coordinateSystem != this.WGS84) {
             let lineResLoc = this.GL.getUniformLocation(this.lineProgram, 'resolutionScale');
             this.GL.uniform2f(lineResLoc, resScaleX, resScaleY);
+        } else {
+            let oneResLoc = this.GL.getUniformLocation(this.lineProgram, 'ONE');
+            this.GL.uniform1f(oneResLoc, 1);
         }
 
         this.GL.useProgram(this.dotProgram);
@@ -574,10 +577,8 @@ class BatchDrawer {
                                     gl_Position = vec4(projection * translate * vertexPos, 1.0);
                                   }`;
             } else {
-                // 64-bit precision float emulation borrowed from:
-                // https://github.com/uber/luma.gl
                 lineVertexSource = `#version 300 es
-                                #define M_PI 3.1415926535897932384626433832795f
+                                #define M_PI 3.1415926535897932384626433832795
                                 #define M_PI_180 0.01745329251f
                                 #define M_2PI 6.28318530718f
 
@@ -593,190 +594,125 @@ class BatchDrawer {
                                 uniform mat3 projection;
                                 uniform float zoomScale;
                                 uniform vec2 pixelOrigin;
-const vec2 E_FP64 = vec2(2.7182817459106445e+00, 8.254840366817007e-08);
-const vec2 LOG2_FP64 = vec2(0.6931471824645996e+00, -1.9046542121259336e-09);
-const vec2 PI_FP64 = vec2(3.1415927410125732, -8.742278012618954e-8);
-const vec2 TWO_PI_FP64 = vec2(6.2831854820251465, -1.7484556025237907e-7);
-const vec2 PI_2_FP64 = vec2(1.5707963705062866, -4.371139006309477e-8);
-const vec2 PI_4_FP64 = vec2(0.7853981852531433, -2.1855695031547384e-8);
-const vec2 PI_16_FP64 = vec2(0.19634954631328583, -5.463923757886846e-9);
-const vec2 PI_16_2_FP64 = vec2(0.39269909262657166, -1.0927847515773692e-8);
-const vec2 PI_16_3_FP64 = vec2(0.5890486240386963, -1.4906100798128818e-9);
-const vec2 PI_180_FP64 = vec2(0.01745329238474369, 1.3519960498364902e-10);
-const vec2 INVERSE_FACTORIAL_3_FP64 = vec2(1.666666716337204e-01, -4.967053879312289e-09); // 1/3!
-const vec2 INVERSE_FACTORIAL_4_FP64 = vec2(4.16666679084301e-02, -1.2417634698280722e-09); // 1/4!
-const vec2 INVERSE_FACTORIAL_5_FP64 = vec2(8.333333767950535e-03, -4.34617203337595e-10); // 1/5!
-const vec2 INVERSE_FACTORIAL_6_FP64 = vec2(1.3888889225199819e-03, -3.3631094437103215e-11); // 1/6!
-const vec2 INVERSE_FACTORIAL_7_FP64 = vec2(1.9841270113829523e-04,  -2.725596874933456e-12); // 1/7!
-const vec2 INVERSE_FACTORIAL_8_FP64 = vec2(2.4801587642286904e-05, -3.406996025904184e-13); // 1/8!
-const vec2 INVERSE_FACTORIAL_9_FP64 = vec2(2.75573188446287533e-06, 3.7935713937038186e-14); // 1/9!
-const vec2 INVERSE_FACTORIAL_10_FP64 = vec2(2.755731998149713e-07, -7.575112367869873e-15); // 1/10!
-vec2 split(float a) {
-  const float SPLIT = 4097.0;
-  float t = a * SPLIT;
-  float a_hi = t - (t - a);
-  float a_lo = a - a_hi;
-  return vec2(a_hi, a_lo);
-}
-float unSplit(vec2 a) {
-  return a.x - a.y;
-}
-vec2 quickTwoSum(float a, float b) {
-  float sum = a + b;
-  float err = b - (sum - a);
-  return vec2(sum, err);
-}
-vec2 twoSum(float a, float b) {
-  float s = (a + b);
-  float v = s - a;
-  float err = (a - (s - v)) + (b - v);
-  return vec2(s, err);
-}
-vec2 twoSub(float a, float b) {
-  float s = (a - b);
-  float v = s - a;
-  float err = (a - (s - v)) - (b + v);
-  return vec2(s, err);
-}
-vec2 twoProd(float a, float b) {
-  float prod = a * b;
-  vec2 a_fp64 = split(a);
-  vec2 b_fp64 = split(b);
-  float err = ((a_fp64.x * b_fp64.x - prod) + a_fp64.x * b_fp64.y +
-    a_fp64.y * b_fp64.x) + a_fp64.y * b_fp64.y;
-  return vec2(prod, err);
-}
-vec2 sum_fp64(vec2 a, vec2 b) {
-  vec2 s, t;
-  s = twoSum(a.x, b.x);
-  t = twoSum(a.y, b.y);
-  s.y += t.x;
-  s = quickTwoSum(s.x, s.y);
-  s.y += t.y;
-  s = quickTwoSum(s.x, s.y);
-  return s;
-}
 
-vec2 sub_fp64(vec2 a, vec2 b) {
-  vec2 s, t;
-  s = twoSub(a.x, b.x);
-  t = twoSub(a.y, b.y);
-  s.y += t.x;
-  s = quickTwoSum(s.x, s.y);
-  s.y += t.y;
-  s = quickTwoSum(s.x, s.y);
-  return s;
-}
-vec2 mul_fp64(vec2 a, vec2 b) {
-  vec2 prod = twoProd(a.x, b.x);
-  // y component is for the error
-  prod.y += a.x * b.y;
-  prod = quickTwoSum(prod.x, prod.y);
-  prod.y += a.y * b.x;
-  prod = quickTwoSum(prod.x, prod.y);
-  return prod;
-}
-vec2 div_fp64(vec2 a, vec2 b) {
-  float xn = 1.0 / b.x;
-  vec2 yn = a * xn;
-  float diff = (sub_fp64(a, mul_fp64(b, yn))).x;
-  vec2 prod = twoProd(xn, diff);
-  return sum_fp64(yn, prod);
-}
-vec2 sin_taylor_fp64(vec2 a) {
-  vec2 r, s, t, x;
-  if (a.x == 0.0 && a.y == 0.0) {
-    return vec2(0.0, 0.0);
-  }
-  x = -mul_fp64(a, a);
-  s = a;
-  r = a;
-  r = mul_fp64(r, x);
-  t = mul_fp64(r, INVERSE_FACTORIAL_3_FP64);
-  s = sum_fp64(s, t);
-  r = mul_fp64(r, x);
-  t = mul_fp64(r, INVERSE_FACTORIAL_5_FP64);
-  s = sum_fp64(s, t);
-  /* keep the following commented code in case we need them
-  for extra accuracy from the Taylor expansion*/
-  // r = mul_fp64(r, x);
-  // t = mul_fp64(r, INVERSE_FACTORIAL_7_FP64);
-  // s = sum_fp64(s, t);
-  // r = mul_fp64(r, x);
-  // t = mul_fp64(r, INVERSE_FACTORIAL_9_FP64);
-  // s = sum_fp64(s, t);
-  return s;
-}
-vec2 exp_fp64(vec2 a) {
-  // We need to make sure these two numbers match
-  // as bit-wise shift is not available in GLSL 1.0
-  const int k_power = 4;
-  const float k = 16.0;
-  const float inv_k = 1.0 / k;
-  if (a.x <= -88.0) return vec2(0.0, 0.0);
-  if (a.x >= 88.0) return vec2(1.0 / 0.0, 1.0 / 0.0);
-  if (a.x == 0.0 && a.y == 0.0) return vec2(1.0, 0.0);
-  if (a.x == 1.0 && a.y == 0.0) return E_FP64;
-  float m = floor(a.x / LOG2_FP64.x + 0.5);
-  vec2 r = sub_fp64(a, mul_fp64(LOG2_FP64, vec2(m, 0.0))) * inv_k;
-  vec2 s, t, p;
-  p = mul_fp64(r, r);
-  s = sum_fp64(r, p * 0.5);
-  p = mul_fp64(p, r);
-  t = mul_fp64(p, INVERSE_FACTORIAL_3_FP64);
-  s = sum_fp64(s, t);
-  p = mul_fp64(p, r);
-  t = mul_fp64(p, INVERSE_FACTORIAL_4_FP64);
-  s = sum_fp64(s, t);
-  p = mul_fp64(p, r);
-  t = mul_fp64(p, INVERSE_FACTORIAL_5_FP64);
-  // s = sum_fp64(s, t);
-  // p = mul_fp64(p, r);
-  // t = mul_fp64(p, INVERSE_FACTORIAL_6_FP64);
-  // s = sum_fp64(s, t);
-  // p = mul_fp64(p, r);
-  // t = mul_fp64(p, INVERSE_FACTORIAL_7_FP64);
-  s = sum_fp64(s, t);
-  // At this point, s = exp(r) - 1; but after following 4 recursions, we will get exp(r) ^ 512 - 1.
-  for (int i = 0; i < k_power; i++) {
-    s = sum_fp64(s * 2.0, mul_fp64(s, s));
-  }
+                                /* sin with 64 bit precision borrowed from luma.gl: https://github.com/uber/luma.gl */
+                                uniform float ONE;
 
-  return s * pow(2.0, m);
-//   return r;
-}
-vec2 log_fp64(vec2 a)
-{
-  if (a.x == 1.0 && a.y == 0.0) return vec2(0.0, 0.0);
-  if (a.x <= 0.0) return vec2(0.0 / 0.0, 0.0 / 0.0);
-  vec2 x = vec2(log(a.x), 0.0);
-  vec2 s;
-  s = vec2(1.0, 0.0);
-  x = sub_fp64(sum_fp64(x, mul_fp64(a, exp_fp64(-x))), s);
-  return x;
-}
+                                #define INTEL_GPU
+                                // Intel optimizes away the calculation necessary for emulated fp64
+                                #define LUMA_FP64_CODE_ELIMINATION_WORKAROUND 1
+                                // Intel's built-in 'tan' function doesn't have acceptable precision
+                                #define LUMA_FP32_TAN_PRECISION_WORKAROUND 1
+                                // Intel GPU doesn't have full 32 bits precision in same cases, causes overflow
+                                #define LUMA_FP64_HIGH_BITS_OVERFLOW_WORKAROUND 1
 
 
+                                float unSplit(vec2 a) {
+                                  return a.x + a.y;
+                                }
+
+                                vec2 split(float a) {
+                                  const float SPLIT = 4097.0;
+                                  float t = a * SPLIT;
+                                #if defined(LUMA_FP64_CODE_ELIMINATION_WORKAROUND)
+                                  float a_hi = t * ONE - (t - a);
+                                  float a_lo = a * ONE - a_hi;
+                                #else
+                                  float a_hi = t - (t - a);
+                                  float a_lo = a - a_hi;
+                                #endif
+                                  return vec2(a_hi, a_lo);
+                                }
+                                vec2 quickTwoSum(float a, float b) {
+                                #if defined(LUMA_FP64_CODE_ELIMINATION_WORKAROUND)
+                                  float sum = (a + b) * ONE;
+                                  float err = b - (sum - a) * ONE;
+                                #else
+                                  float sum = a + b;
+                                  float err = b - (sum - a);
+                                #endif
+                                  return vec2(sum, err);
+                                }
+                                vec2 twoSum(float a, float b) {
+                                  float s = (a + b);
+                                #if defined(LUMA_FP64_CODE_ELIMINATION_WORKAROUND)
+                                  float v = (s * ONE - a) * ONE;
+                                  float err = (a - (s - v) * ONE) * ONE * ONE * ONE + (b - v);
+                                #else
+                                  float v = s - a;
+                                  float err = (a - (s - v)) + (b - v);
+                                #endif
+                                  return vec2(s, err);
+                                }
+
+                                vec2 twoProd(float a, float b) {
+                                  float prod = a * b;
+                                  vec2 a_fp64 = split(a);
+                                  vec2 b_fp64 = split(b);
+                                  float err = ((a_fp64.x * b_fp64.x - prod) + a_fp64.x * b_fp64.y +
+                                    a_fp64.y * b_fp64.x) + a_fp64.y * b_fp64.y;
+                                  return vec2(prod, err);
+                                }
+                                vec2 sum_fp64(vec2 a, vec2 b) {
+                                  vec2 s, t;
+                                  s = twoSum(a.x, b.x);
+                                  t = twoSum(a.y, b.y);
+                                  s.y += t.x;
+                                  s = quickTwoSum(s.x, s.y);
+                                  s.y += t.y;
+                                  s = quickTwoSum(s.x, s.y);
+                                  return s;
+                                }
+
+                                vec2 mul_fp64(vec2 a, vec2 b) {
+                                  vec2 prod = twoProd(a.x, b.x);
+                                  // y component is for the error
+                                  prod.y += a.x * b.y;
+                                  prod.y += a.y * b.x;
+                                  prod = quickTwoSum(prod.x, prod.y);
+                                  return prod;
+                                }
+
+                                const vec2 INVERSE_FACTORIAL_3_FP64 = vec2(1.666666716337204e-01, -4.967053879312289e-09); // 1/3!
+                                const vec2 INVERSE_FACTORIAL_5_FP64 = vec2(8.333333767950535e-03, -4.34617203337595e-10); // 1/5!
+                                const vec2 INVERSE_FACTORIAL_7_FP64 = vec2(1.9841270113829523e-04,  -2.725596874933456e-12); // 1/7!
+                                const vec2 INVERSE_FACTORIAL_9_FP64 = vec2(2.75573188446287533e-06, 3.7935713937038186e-14); // 1/9!
+
+                                vec2 sin_taylor_fp64(vec2 a) {
+                                  vec2 r, s, t, x;
+                                  if (a.x == 0.0 && a.y == 0.0) {
+                                    return vec2(0.0, 0.0);
+                                  }
+                                  x = -mul_fp64(a, a);
+                                  s = a;
+                                  r = a;
+                                  r = mul_fp64(r, x);
+                                  t = mul_fp64(r, INVERSE_FACTORIAL_3_FP64);
+                                  s = sum_fp64(s, t);
+                                  r = mul_fp64(r, x);
+                                  t = mul_fp64(r, INVERSE_FACTORIAL_5_FP64);
+                                  s = sum_fp64(s, t);
+                                  r = mul_fp64(r, x);
+                                  t = mul_fp64(r, INVERSE_FACTORIAL_7_FP64);
+                                  s = sum_fp64(s, t);
+                                  r = mul_fp64(r, x);
+                                  t = mul_fp64(r, INVERSE_FACTORIAL_9_FP64);
+                                  s = sum_fp64(s, t);
+                                  return s;
+                                }
+
+                                float sin_64(float a) {
+                                  return unSplit(sin_taylor_fp64(split(a)));
+                                }
+
+                                /* Mercator projection using 64 bit sin function above */
                                 vec2 wgs84_to_webmerc(vec2 latlong) {
-                                    vec2 pX64;
-                                    vec2 pY64;
-                                    vec2 p;
-                                    vec2 one = split(1.f);
-                                    vec2 zoomScale64 = split(zoomScale);
-                                    vec2 pi_180 = split(M_PI_180);
-                                    vec2 pi_2 = split(M_2PI);
-                                    vec2 lat = split(latlong.y);
-                                    vec2 lon = split(latlong.x);
-
-                                    vec2 sin_lat = sin_taylor_fp64(mul_fp64(pi_180, lat));
-                                    pX64 = sum_fp64(div_fp64(mul_fp64(zoomScale64,  lon), split(180.f)), split(zoomScale));
-
-                                    // atanh:
-                                    vec2 logTerm = log_fp64(div_fp64(sub_fp64(one, sin_lat), sum_fp64(one, sin_lat)));
-                                    pY64 = mul_fp64(zoomScale64, sum_fp64(div_fp64(logTerm, pi_2), one));
-
-//                                    p.y = zoomScale * (-log((1.f + sin_lat) / (1.f - sin_lat)) / (2.f * M_PI) + 1.f);
-                                    return vec2(unSplit(pX64), unSplit(pY64));
+                                  vec2 p;
+                                  float sin_lat = sin_64(latlong.y * M_PI_180);
+                                  p.x = zoomScale * (latlong.x / 180.f + 1.f);
+                                  // atanh:
+                                  p.y = zoomScale * (-log((1.f + sin_lat) / (1.f - sin_lat)) / (M_2PI) + 1.f);
+                                  return p;
                                 }
 
                                 void main(void) {
